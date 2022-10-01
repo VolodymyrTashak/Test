b83532bb836797e9fd9321e1b4b15d45219c2735
VolodymyrTashak <vovavozny33@gmail.com>
2022-10-01 20:07:50 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-import axios from 'axios';
-
-const KEY = '77e7936073a1f82fbc0d3a17a985fb5b';
-// Запит на отримання популярних фільмів
-function fetchMoviesTrend() {
-  const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${KEY}`;
-  fetch(url).then(r => {
-    console.log(r);
-  });
-}
-fetchMoviesTrend();
-
-// import { NewApiService } from './js/newsApiService';
-
-// const newsApiService = new NewApiService();
-// const cardList = document.querySelector('.card__list');
-
-// async function searchCard(e) {
-//   e.preventDefault();
-//   newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-
-//   const res = await newsApiService.fetchMoviesTrend();
-//   //   const response = await NewApiService(searchName);
-//   //   console.log(response);
-// }
+(() => {
+  const refs = {
+    openStudentsBtn: document.querySelector('.students-button__open'),
+    closeStudentsBtn: document.querySelector('.students-button__close'),
+  };
+})();
--- src/js/renderList.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const gallery = document.querySelector('.card__list');
-
-export function renderList(data) {
-  const render = data.map(card => {});
-}
