ec971f6d3cdb891ff00e4710708ab8d8847a8664
VolodymyrTashak <99536839+VolodymyrTashak@users.noreply.github.com>
2022-10-01 17:08:26 +0000

    Deploying to gh-pages from @ VolodymyrTashak/Test@b83532bb836797e9fd9321e1b4b15d45219c2735 🚀

---
+++ index.5097f762.js
@@ -0,0 +1,2 @@
+document.querySelector(".students-button__open"),document.querySelector(".students-button__close");
+//# sourceMappingURL=index.5097f762.js.map
---
+++ index.c1d47cdb.js
@@ -0,0 +1,2 @@
+document.querySelector(".students-button__open"),document.querySelector(".students-button__close");
+//# sourceMappingURL=index.c1d47cdb.js.map
